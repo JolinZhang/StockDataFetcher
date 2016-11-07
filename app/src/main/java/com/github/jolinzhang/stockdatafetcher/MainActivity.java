0b19ffb50e9c2090d60efd4f7c615ce1efd37f1e
ShaneQi <qizengtai@gmail.com>
2016-11-07 16:44:15 -0600

    Updated .gitignore.

--- app/src/main/java/com/github/jolinzhang/stockdatafetcher/MainActivity.java
+++ app/src/main/java/com/github/jolinzhang/stockdatafetcher/MainActivity.java
@@ -3,11 +3,29 @@ package com.github.jolinzhang.stockdatafetcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.jolinzhang.tablefetcher.FetchResult;
+import com.github.jolinzhang.tablefetcher.ITableFetcher;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ITableFetcher fetcher = null;
+        fetcher.fetch("stock", new ITableFetcher.IFetchResultHandler() {
+            @Override
+            public void handle(FetchResult result) {
+                switch (result) {
+                    case success:
+
+                        break;
+                    case failure:
+
+                        break;
+                }
+            }
+        });
     }
 }
